eb3e84f270c69ca30b18f4168085af2c08dcd516
Martin Njau <njau_martin@yahoo.comgit config --global user.email njau_martin@yahoo.comclearclear>
2023-10-28 17:18:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
+
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+  //let books = data
+  //console.log(books)
+    renderBooks(data);
   
+  });
 }
+  
+
+  //let books = data.map(data => ({ name: value }));
+  //console.log(books);
+  //});
+   // const books = () => {
+      //return data.map(data => ({ name: book.title }));
+     //};
+    // console.log(books);
+   //let newArray = data.map(data=> ({name: book. name}));
+   //let books = {};
+   //for(let i=0; i<newArray.length; i++) {
+    //books[i+1] = newArray[i];
+   //}
+  // return(books);
+    //console.log(data.name);
+  
+   // let books = data.title;
+   //console.log(data);
+   // return books;
+  //});
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
